595ba40d95bfac23ce3caaa886214724728aa4c7
Po-An Tsai <poant@nvidia.com>
2019-10-10 14:47:05 -0700

    [accelergy_interface] better error message

--- src/util/accelergy_interface.hpp
+++ src/util/accelergy_interface.hpp
@@ -11,7 +11,7 @@ namespace accelergy
     cmd += " -o ./ > ./accelergy.log 2>&1";
     int ret = system(cmd.c_str());
     if (ret) {
-      std::cout << "Cannot invoke Accelergy. Do you specify ACCELERGYPATH correctly?" << std::endl;
+      std::cout << "Cannot invoke Accelergy. Do you specify ACCELERGYPATH correctly? Or check accelergy.log to see what went wrong" << std::endl;
       exit(0);
     }
 #else
