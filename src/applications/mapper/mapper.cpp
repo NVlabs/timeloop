cf670a27d78bd92b9d1f1b1f5a9500df54ecb453
tanner-andrulis <81568882+tanner-andrulis@users.noreply.github.com>
2022-06-13 09:05:36 -0400

    Added options for scientific or default floating point outputs (#144)

--- src/applications/mapper/mapper.cpp
+++ src/applications/mapper/mapper.cpp
@@ -529,21 +529,21 @@ void Application::Run()
     if (!sparse_optimizations_->no_optimization_applied)
     {
       std::cout << "Summary stats for best mapping found by mapper:" << std::endl;
-      std::cout << "  Utilization = " << std::setw(4) << std::fixed << std::setprecision(2)
+      std::cout << "  Utilization = " << std::setw(4) << OUT_FLOAT_FORMAT << std::setprecision(2)
                 << global_best_.stats.utilization << " | pJ/Algorithmic-Compute = " << std::setw(8)
-                << std::fixed << PRINTFLOAT_PRECISION << global_best_.stats.energy /
+                << OUT_FLOAT_FORMAT << PRINTFLOAT_PRECISION << global_best_.stats.energy /
         global_best_.stats.algorithmic_computes
                 << " | pJ/Compute = " << std::setw(8)
-                << std::fixed << PRINTFLOAT_PRECISION << global_best_.stats.energy /
+                << OUT_FLOAT_FORMAT << PRINTFLOAT_PRECISION << global_best_.stats.energy /
         global_best_.stats.actual_computes << std::endl;
     }
     else
     {
       std::cout << "Summary stats for best mapping found by mapper:" << std::endl;
-      std::cout << "  Utilization = " << std::setw(4) << std::fixed << std::setprecision(2)
+      std::cout << "  Utilization = " << std::setw(4) << OUT_FLOAT_FORMAT << std::setprecision(2)
                 << global_best_.stats.utilization
                 << " | pJ/Compute = " << std::setw(8)
-                << std::fixed << PRINTFLOAT_PRECISION << global_best_.stats.energy /
+                << OUT_FLOAT_FORMAT << PRINTFLOAT_PRECISION << global_best_.stats.energy /
         global_best_.stats.actual_computes << std::endl;
     }
     
