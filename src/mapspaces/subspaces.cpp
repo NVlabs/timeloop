NVlabs/timeloop

commit a9c5115f6c595abb0e5352aa3300cfa53a5d581a

- Fix factor-parsing bug in constraints and mapping parsing logic.   A comment without a leading whitespace was being parsed   incorrectly. - Add additional sanity checks to IndexFactorization and Factors   classes.

src/mapspaces/subspaces.cpp
@@ -46,6 +46,37 @@ void IndexFactorizationSpace::Init(const problem::Workload &workload,
                                    std::map<problem::Shape::FlattenedDimensionID, std::map<unsigned, unsigned long>> prefactors,
                                    std::map<problem::Shape::FlattenedDimensionID, std::map<unsigned, unsigned long>> maxfactors)
 {
+  // Sanity check on input pre-factors.
+  for (unsigned idim = 0; idim < unsigned(problem::GetShape()->NumFlattenedDimensions); idim++)
+  {
+    unsigned long bound = workload.GetFlattenedBound(idim);
+
+    unsigned long product = 1;
+    for (auto& entry: prefactors.at(idim))
+    {
+      product *= entry.second;
+    }
+
+    if ((bound % product) != 0)
+    {
+      std::cerr << "ERROR: IndexFactorization: workload bound "
+                << problem::GetShape()->FlattenedDimensionIDToName.at(idim) << " = "
+                << bound << " is not divisible by product of user-provided constraint factors = "
+                << product << std::endl;
+      std::exit(1);
+    }
+
+    if ((cofactors_order.at(idim) == prefactors.at(idim).size()) && product != bound)
+    {
+      std::cerr << "ERROR: IndexFactorization: product of fully-user-provided constraint factors "
+                << product << " is not equal to workload bound "
+                << problem::GetShape()->FlattenedDimensionIDToName.at(idim) << " = "
+                << bound << std::endl;
+      std::exit(1);
+    }
+  }  
+
+  // Create factor sets.
   problem::PerFlattenedDimension<uint128_t> counter_base;
   for (int idim = 0; idim < int(problem::GetShape()->NumFlattenedDimensions); idim++)
   {

src/mapping/constraints.cpp
@@ -593,6 +593,9 @@ void Constraints::ParseSingleConstraint(
         exit(1);
       }
       factors_[level_id][factor.first] = factor.second;
+      // std::cout << "Parsing factor level = " << arch_props_.TilingLevelName(level_id)
+      //           << " dim = " << problem::GetShape()->FlattenedDimensionIDToName.at(factor.first)
+      //           << " factor = " << factor.second << std::endl;
     }
 
     auto level_max_factors = ParseMaxFactors(attributes);
@@ -877,6 +880,8 @@ Constraints::ParseFactors(config::CompoundConfigNode constraint)
   std::string buffer;
   if (constraint.lookupValue("factors", buffer))
   {
+    buffer = buffer.substr(0, buffer.find("#"));
+
     std::regex re("([A-Za-z]+)[[:space:]]*[=]*[[:space:]]*([0-9]+)", std::regex::extended);
     std::smatch sm;
     std::string str = std::string(buffer);
@@ -936,6 +941,8 @@ Constraints::ParseMaxFactors(config::CompoundConfigNode constraint)
   std::string buffer;
   if (constraint.lookupValue("factors", buffer))
   {
+    buffer = buffer.substr(0, buffer.find("#"));
+
     std::regex re("([A-Za-z]+)[[:space:]]*<=[[:space:]]*([0-9]+)", std::regex::extended);
     std::smatch sm;
     std::string str = std::string(buffer);

src/mapping/parser.cpp
@@ -552,6 +552,8 @@ std::map<problem::Shape::FlattenedDimensionID, std::pair<int,int>> ParseUserFact
   std::string buffer;
   if (directive.lookupValue("factors", buffer))
   {
+    buffer = buffer.substr(0, buffer.find("#"));
+
     std::regex re("([A-Za-z]+)[[:space:]]*[=]*[[:space:]]*([0-9]+)(,([0-9]+))?", std::regex::extended);
     std::smatch sm;
     std::string str = std::string(buffer);

src/util/numeric.cpp
@@ -80,6 +80,11 @@ Factors::MultiplicativeSplitRecursive_(unsigned long n, int order)
 {
   if (order == 0)
   {
+    if (n != 1)
+    {
+      std::cerr << "ERROR: Factors: cannot split n=" << n << " into 0 cofactors." << std::endl;
+      assert(false);
+    }
     return {{}};
   }
   else if (order == 1)

commit 66a2dd0dbc8390514c5db9967222378ab6662307

Fix sanity-checking bug introduced in a9c5115f.

src/mapspaces/subspaces.cpp
@@ -47,30 +47,32 @@ void IndexFactorizationSpace::Init(const problem::Workload &workload,
                                    std::map<problem::Shape::FlattenedDimensionID, std::map<unsigned, unsigned long>> maxfactors)
 {
   // Sanity check on input pre-factors.
-  for (unsigned idim = 0; idim < unsigned(problem::GetShape()->NumFlattenedDimensions); idim++)
+  for (auto& prefactor_set: prefactors)
   {
-    unsigned long bound = workload.GetFlattenedBound(idim);
+    auto dim = prefactor_set.first;
 
     unsigned long product = 1;
-    for (auto& entry: prefactors.at(idim))
+    for (auto& prefactor: prefactor_set.second)
     {
-      product *= entry.second;
+      product *= prefactor.second;
     }
+
+    unsigned long bound = workload.GetFlattenedBound(dim);
 
     if ((bound % product) != 0)
     {
       std::cerr << "ERROR: IndexFactorization: workload bound "
-                << problem::GetShape()->FlattenedDimensionIDToName.at(idim) << " = "
+                << problem::GetShape()->FlattenedDimensionIDToName.at(dim) << " = "
                 << bound << " is not divisible by product of user-provided constraint factors = "
                 << product << std::endl;
       std::exit(1);
     }
 
-    if ((cofactors_order.at(idim) == prefactors.at(idim).size()) && product != bound)
+    if ((cofactors_order.at(dim) == prefactor_set.second.size()) && product != bound)
     {
       std::cerr << "ERROR: IndexFactorization: product of fully-user-provided constraint factors "
                 << product << " is not equal to workload bound "
-                << problem::GetShape()->FlattenedDimensionIDToName.at(idim) << " = "
+                << problem::GetShape()->FlattenedDimensionIDToName.at(dim) << " = "
                 << bound << std::endl;
       std::exit(1);
     }