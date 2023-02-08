e1ce60cd4575a6b5126b254c86606f68108f03c7
Michael Gilbert <gilbertm@mit.edu>
2023-02-08 16:47:31 -0500

    wip

--- src/loop-analysis/mapping-to-isl.cpp
+++ src/loop-analysis/mapping-to-isl.cpp
@@ -21,23 +21,26 @@ namespace analysis
 std::map<DataSpaceID, IslMap>
 OpsToDSpaceFromEinsum(const problem::Workload& workload);
 
-/**
- * @brief Compute iteration -> operation map for every branch in mapping
- * 
- * @param nest 
- * @return std::map<mapping::NodeID, IslMap> 
- */
-std::map<mapping::NodeID, IslMap>
-TilingFromMapping(const mapping::FusedMapping& mapping);
 
 /**
- * @brief Compute iteration -> operation map. NodeID is always 0.
- * 
- * @param nest 
- * @return std::map<mapping::NodeID, IslMap> 
+ * @brief Utility to help TilingFromMapping track coefficients.
  */
-std::map<mapping::NodeID, IslMap>
-TilingFromMapping(const Mapping& nest);
+struct TilingCoefTracker
+{
+  TilingCoefTracker();
+
+  TilingCoefTracker&
+  NewIterDim(const problem::Shape::FlattenedDimensionID& op_dim,
+             const std::optional<size_t>& coef);
+
+ private:
+  friend IslMap TilingCoefTrackerToMap(TilingCoefTracker&& tracker);
+
+  std::vector<std::vector<std::optional<size_t>>> coefs_;
+};
+
+IslMap TilingCoefTrackerToMap(TilingCoefTracker&& tracker);
+
 
 /******************************************************************************
  * Global function implementations
@@ -50,8 +53,56 @@ LogicalBufOccupancies OccupanciesFromMapping(const Mapping& nest,
   auto branch_tiling = TilingFromMapping(nest);
 }
 
+
 /******************************************************************************
  * Local function implementations
  *****************************************************************************/
+
+BranchTilings
+TilingFromMapping(const mapping::FusedMapping& mapping,
+                  const problem::Workload& workload)
+{
+  BranchTilings result;
+  for (const auto& path : GetPaths(mapping))
+  {
+    TilingCoefTracker coef_tracker;
+    std::optional<IslPwMultiAff> explicit_tiling_spec;
+    mapping::NodeID leaf_id;
+    for (const auto& node : path)
+    {
+      std::visit(
+        [&coef_tracker, &explicit_tiling_spec, &leaf_id] (auto&& node) {
+          using NodeT = std::decay_t<decltype(node)>;
+          if constexpr (std::is_same_v<NodeT, mapping::For>
+                        || std::is_same_v<NodeT, mapping::ParFor>)
+          {
+            coef_tracker.NewIterDim(node.op_dim, node.end);
+          } else if constexpr (std::is_same_v<NodeT, mapping::Compute>)
+          {
+            explicit_tiling_spec = node.tiling_spec;
+            leaf_id = node.id;
+          }
+        },
+        node
+      );
+    }
+
+    if (explicit_tiling_spec)
+    {
+      result.emplace(std::make_pair(
+        leaf_id,
+        IslMap::FromMultiAff(std::move(*explicit_tiling_spec))
+      ));
+    } else
+    {
+      result.emplace(std::make_pair(
+        leaf_id,
+        TilingCoefTrackerToMap(std::move(coef_tracker))
+      ));
+    }
+  }
+
+  return result;
+}
 
 };
