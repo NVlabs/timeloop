ff9081d350deb64fc2d406dd5a8dc5ca582aa485
Michael Gilbert <gilbertm@mit.edu>
2023-06-22 17:24:14 -0400

    [loop-analysis] Unify link transfer and multicast model interface.

--- include/loop-analysis/isl-nest-analysis.hpp
+++ include/loop-analysis/isl-nest-analysis.hpp
@@ -9,15 +9,31 @@ namespace analysis
 struct LogicalBufferStats
 {
   const LogicalBuffer& buf;
+
+  /// @brief The occupancy of the buffer at a point in space-time.
   Occupancy occupancy;
+
+  /**
+   * @brief The occupancy of the buffer at a point in space-time where reuse
+   *   temporal dimensions have been pruned.
+   */
   Occupancy effective_occupancy;
+
+  /// @brief The amount of fills the buffer requires at a point in space-time.
   Fill fill;
+
+  /// @brief The amount of link transfers going in at a point in space-time.
   Transfers link_transfer;
+
+  /// @brief Reads going out to parent buffer
   Reads parent_reads;
 
   /***************** Compatibility with Timeloop v2.0 ************************/
 
-  std::map<std::pair<uint64_t, uint64_t>, MulticastInfo::AccessStats>
+  /**
+   * @see analysis::DataMovementInfo
+   */
+  std::map<std::pair<uint64_t, uint64_t>, TransferInfo::AccessStats>
   compat_access_stats;
 
   /***************************************************************************/
