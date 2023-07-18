fd7d9fc2a2543932f9a8f5a151f6dd5744ae49c8
Jenny Huang <jennyhuang@nvidia.com>
2023-07-18 04:45:50 -0700

    Add options for oaves post processing

--- scripts/oaves_process_data.py
+++ scripts/oaves_process_data.py
@@ -37,30 +37,28 @@ def construct_argparser():
     return parser
 
 
-def process_data(stats_file: str, output_file: str, keep_all_entry: bool = False, keep_one_best_entry: bool = False):
+def process_data(stats_file: str, output_file: str, keep_all_entry: bool = False, keep_one_best_entry: bool = False, keep_one_best_entry_across_buf: bool = False):
     """ Process OAVES output csv file.
 
     Args:
 	stats_file: The OAVES output csv file path from Timeloop run.
 	output_file: The output file path to store sorted data.
-        keep_all_entry: Indicate whether to keep all different buffer sizes.
-        keep_all_entry: Indicate whether to keep all different buffer sizes.
-
+        keep_all_entry: Indicate whether to keep all different buffer sizes and the corresponding optimal mappings.
+        keep_one_best_entry: Indicate whether to keep only the best OI and DRAM accesses per buffer size.
+        keep_one_best_entry_across_buf: Indicate whether to keep only the best OI and DRAM accesses accross various buffer sizes.
     """
 
     df = pd.read_csv(stats_file, header=None)
-    generated_mapping_files = set(df.iloc[:,-1])
+    generated_mapping_files = set(df.iloc[:,4])
 
     # Columns are defined as follows:
-    # 0 - buffer size, 1 - op intensity, 2 - dram word accesses, 3,4,5 - buffer size for each tensor, 6,7,8 - dram word accesses for each tensor, 9 - compact print of mapping, 10 - path to mapping.yaml
     # 0-total buffer utilization (B), 1-DRAM operation intensity (op/B), 2-DRAM accesses (word), 3-compact mapping, 4-mapping file path,
     # 5: total operations, 6:6+m*3-per tensor utilization (B), 6+m*3:6+m*3+3-per tensor DRAM accesses (word)
 
     # group by the buf util index 0 and take the max values of op int at index 1 and find the idx of max values
-
-    # idx = df.groupby(0)[1].idxmax()
+    # find mapping that matters
+    # idx = df.groupby(1)[0].idxmin()
     # df = df.loc[idx]
-    # print(len(df))
     if keep_one_best_entry:
         idx = df.groupby(0)[1].idxmax()
         df = df.loc[idx]
@@ -70,36 +68,43 @@ def process_data(stats_file: str, output_file: str, keep_all_entry: bool = False
 
     df = df.sort_values(by=[0])
 
-
     if keep_all_entry:
         df_new = df
 
     # only save the mapping that lead to higher op intensity with higher buf utilization
     else:
-        max_op_int_val = 0
-        max_index = []
-        for i, row in df.iterrows():
-            cur_val = row[1]
-            if cur_val >= max_op_int_val:
-                max_op_int_val = cur_val
-                max_index.append(i)
-        df_new = df.loc[max_index]
+        if keep_one_best_entry_across_buf:
+            max_op_int_val = 0
+            max_index = []
+            for i, row in df.iterrows():
+                cur_val = row[1]
+                if cur_val > max_op_int_val:
+                    max_op_int_val = cur_val
+                    max_index.append(i)
+            df_new = df.loc[max_index]
+        else:
+            df_new = df
 
         # Delete the non-optimal mapping yaml files
-        optimal_mapping_files = set(df_new.iloc[:,4])
-        mapping_files_to_delete = generated_mapping_files - optimal_mapping_files
-        for mapping_file in mapping_files_to_delete:
-            if os.path.isfile(mapping_file):
-                try:
-                    os.remove(mapping_file)
-                except OSError as e:
-                    print (f"Failed to remove suboptimal mapping file {mapping_file}!")
-                    print (e.code, e.strerror)
-        # Check if all optimal mapping yamls exist
-        for mapping_file in optimal_mapping_files:
-            if mapping_file != 'None' and not os.path.isfile(mapping_file):
-                raise Exception(f"Optimal mapping file {mapping_file} not found!")
+        try:
+            optimal_mapping_files = set(df_new.iloc[:,4])
+            mapping_files_to_delete = generated_mapping_files - optimal_mapping_files
+            for mapping_file in mapping_files_to_delete:
+                print(mapping_file)
+                if os.path.isfile(mapping_file):
+                    try:
+                        os.remove(mapping_file)
+                    except OSError as e:
+                        print (f"Failed to remove suboptimal mapping file {mapping_file}!")
+                        print (e.code, e.strerror)
+            # Check if all optimal mapping yamls exist
+            for mapping_file in optimal_mapping_files:
+                if mapping_file != 'None' and not os.path.isfile(mapping_file):
+                    raise Exception(f"Optimal mapping file {mapping_file} not found!")
+        except:
+            pass
 
+    # df_drop = df.drop(df.columns[4], axis=1)
     df_new = df_new.set_index(0)
     df_new.to_csv(output_file, header=False)
 
