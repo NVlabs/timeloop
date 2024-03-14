40934455e5b0b5c16961718f24d34f0aa5eb618c
Michael Gilbert <gilbertm@mit.edu>
2024-03-14 16:49:34 -0400

    [loop-analysis] Draft multiple loop reuse

--- src/loop-analysis/temporal-analysis.cpp
+++ src/loop-analysis/temporal-analysis.cpp
@@ -12,7 +12,7 @@ namespace analysis
  * Local declarations
  *****************************************************************************/
 
-std::pair<Occupancy, Fill> FillFromOccupancy(const Occupancy&);
+std::pair<Occupancy, Fill> FillFromOccupancy(const Occupancy&, bool);
 
 /******************************************************************************
  * Global function implementations
@@ -30,9 +30,10 @@ TemporalReuseAnalysis(TemporalReuseAnalysisInput input)
 {
   auto exploit_temporal_reuse = input.reuse_opts.exploit_temporal_reuse;
   const auto& occupancy = input.occupancy;
+  bool multi_loop_reuse = input.reuse_opts.multi_loop_reuse;
 
   if (exploit_temporal_reuse){
-    auto [eff_occ, fill] = FillFromOccupancy(occupancy);
+    auto [eff_occ, fill] = FillFromOccupancy(occupancy, multi_loop_reuse);
     return TemporalReuseAnalysisOutput{
       .effective_occupancy = std::move(eff_occ),
       .fill = std::move(fill)
@@ -51,7 +52,8 @@ TemporalReuseAnalysis(TemporalReuseAnalysisInput input)
  * Local function implementations
  *****************************************************************************/
 
-std::pair<Occupancy, Fill> FillFromOccupancy(const Occupancy& occupancy)
+std::pair<Occupancy, Fill> FillFromOccupancy(const Occupancy& occupancy,
+					     bool multi_loop_reuse)
 {
   using namespace boost::adaptors;
 
@@ -70,6 +72,7 @@ std::pair<Occupancy, Fill> FillFromOccupancy(const Occupancy& occupancy)
       continue;
     }
 
+    // Check if temporal dimension is "trivial," i.e., equals a singular value
     auto p_proj_occ =
       isl_map_project_out(isl_map_copy(p_occ), isl_dim_in, dim_idx, 1);
     auto p_reinserted_occ = isl_map_intersect_domain(
@@ -90,12 +93,28 @@ std::pair<Occupancy, Fill> FillFromOccupancy(const Occupancy& occupancy)
     isl_map_free(p_proj_occ);
     isl_map_free(p_reinserted_occ);
 
-    auto p_time_shift = isl::map_to_shifted(
-      isl_space_domain(isl_map_get_space(p_occ)),
-      dim_idx,
-      -1
-    );
-    auto p_occ_before = isl_map_apply_range(p_time_shift, isl_map_copy(p_occ));
+    isl_map* p_time_shift = nullptr;
+    if (!multi_loop_reuse)
+    {
+      p_time_shift = isl::map_to_shifted(
+        isl_space_domain(isl_map_get_space(p_occ)),
+	dim_idx,
+	-1
+      );
+    }
+    else
+    {
+      isl_set* p_spacetime_domain = isl_map_domain(isl_map_copy(p_occ));
+      isl_map* p_spacetime_domain_to_time_domain; // TODO
+      isl_map* p_time_domain_to_past = isl_map_lex_gt(isl_set_get_space(isl_map_range(isl_map_copy(p_spacetime_domain_to_time_domain))));
+      p_time_shift = isl_map_apply_range(
+	isl_map_lexmax(p_time_domain_to_past),
+	isl_map_reverse(p_spacetime_domain_to_time_domain)
+      );
+    }
+
+    auto p_occ_before = isl_map_apply_range(p_time_shift,
+					    isl_map_copy(p_occ));
     auto p_fill = isl_map_subtract(isl_map_copy(p_occ), p_occ_before);
 
     return std::make_pair(
@@ -107,6 +126,5 @@ std::pair<Occupancy, Fill> FillFromOccupancy(const Occupancy& occupancy)
   return std::make_pair(Occupancy(tags, isl::manage(isl_map_copy(p_occ))),
                         Fill(tags, isl::manage(p_occ)));
 }
-
 
 } // namespace analysis
